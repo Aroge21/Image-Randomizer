01fecee5622b56d53e742e62811b5789457d24fe
Aroge21 <82720686+Aroge21@users.noreply.github.com>
2023-09-26 11:50:10 -0500

    Made Comments to main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import numpy as np
 import random
 import math
 
+# Sets size of screen
 width = 400
 height = 300
 number = 0
 
 pixels = []
 
+# Initates colors for each varible
 red = 0
 green = 0
 blue = 0
 
+# Makes a txt file to label new pictures correctly
 try:
     with open('img_num.txt') as docNum:
         number = int(docNum.read())
@@ -20,6 +23,7 @@ except:
     with open('img_num.txt', 'w') as docNum:
         docNum.write(str(number))
 
+# Goes through every pixel in a 2D array
 while 1:
     pixels = []
     counter = 0
@@ -29,28 +33,29 @@ while 1:
         temp = []
 
         while counter < width:
+            # Makes random colors for each pixel in screen size of array
             red = random.randint(0, 256)
             green = random.randint(0, 256)
             blue = random.randint(0, 256)
 
+            #Sets random colored varibles into rgb array
             rgb = [red, green, blue]
 
             temp.append(rgb)
             counter += 1
         
         pixels.append(temp)
-    
-    ##print(pixels)
 
+    # Sets the 2D array of of all random pixels
     array = np.array(pixels, dtype=np.uint8)
 
+    # Makes image from 2D array
     new_img = Image.fromarray(array)
-    ##new_img.show()
+
+    # Saves images to whatevery folder you want
     new_img.save(f'image/new_image{number}.bmp')
 
     number += 1
 
     with open('img_num.txt', 'w') as docNum:
         docNum.write(str(number))
-
-
